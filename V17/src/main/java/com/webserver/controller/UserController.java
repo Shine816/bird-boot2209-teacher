817d96b88c43bba156d6c93a89f3d62311b01809
TEDU <83123095@qq.com>
2022-11-18 09:14:45 +0800

    day01

---
+++ V17/src/main/java/com/webserver/controller/UserController.java
@@ -0,0 +1,76 @@
+package com.webserver.controller;
+
+import com.webserver.annotations.Controller;
+import com.webserver.annotations.RequestMapping;
+import com.webserver.entity.User;
+import com.webserver.http.HttpServletRequest;
+import com.webserver.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.URISyntaxException;
+
+/**
+ * 用于处理用户相关业务
+ */
+@Controller
+public class UserController {
+    private static File userDir;
+    static {
+        userDir = new File("./users");
+        if(!userDir.exists()){
+            userDir.mkdirs();
+        }
+    }
+
+    @RequestMapping("/regUser")
+    public void reg(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("开始处理用户注册!!!!!!!!!!");
+        //对应的是reg.html页面上<input name="username">
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String nickname = request.getParameter("nickname");
+        String ageStr = request.getParameter("age");
+        //必要验证工作
+        if(username==null||username.isEmpty()||password==null||password.isEmpty()||
+                nickname==null||nickname.isEmpty()||ageStr==null||ageStr.isEmpty()||
+                !ageStr.matches("[0-9]+")){
+            //要求浏览器查看错误提示页面
+            response.sendRedirect("/reg_info_error.html");
+            return;
+
+        }
+        System.out.println(username+","+password+","+nickname+","+ageStr);
+
+        int age = Integer.parseInt(ageStr);
+        //2
+        //在userDir表示的目录中创建指定名字的文件
+        File file = new File(userDir,username+".obj");
+        //检查是否为重复用户
+        if(file.exists()){//若该文件已经存在了说明是已经注册过的用户了
+            response.sendRedirect("/have_user.html");
+            return;
+        }
+
+        User user = new User(username,password,nickname,age);
+        try (
+                FileOutputStream fos = new FileOutputStream(file);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ){
+            oos.writeObject(user);
+            //使用响应对象要求浏览器查看注册成功页面
+            response.sendRedirect("/reg_success.html");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping("/loginUser")
+    public void login(HttpServletRequest request,HttpServletResponse response){
+        System.out.println("开始处理登录!!!");
+    }
+}
---
+++ V17/src/main/java/com/webserver/core/DispatcherServlet.java
@@ -0,0 +1,111 @@
+package com.webserver.core;
+
+import com.webserver.annotations.Controller;
+import com.webserver.annotations.RequestMapping;
+import com.webserver.controller.UserController;
+import com.webserver.http.HttpServletRequest;
+import com.webserver.http.HttpServletResponse;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+/**
+ * DispatcherServlet实际是由SpringMVC框架提供的一个类，用于和Tomcat整合并负责
+ * 接手处理请求的工作。
+ * <p>
+ * Servlet是JAVA EE里的一个接口，译作:运行在服务端的小程序
+ * Servlet中有一个重要的抽象方法:
+ * public void service(HttpServletRequest request,HttpServletResponse response)
+ * 该方法用于处理某个服务
+ * <p>
+ * SpringMVC框架提供的DispatcherServlet就实现了该接口并重写了service方法，那么与Tomcat整合后，Tomcat在处理
+ * 请求的环节就可以调用DispatcherServlet的service方法将请求对象与响应对象传递进去由SpringMVC框架完成处理请求
+ * 的操作。
+ */
+public class DispatcherServlet {
+    private static DispatcherServlet instance = new DispatcherServlet();
+    private static File root;
+    private static File staticDir;
+
+    static {
+        try {
+            root = new File(
+                    DispatcherServlet.class.getClassLoader().getResource(".").toURI()
+            );
+            staticDir = new File(root, "static");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DispatcherServlet() {
+    }
+
+    public static DispatcherServlet getInstance() {
+        return instance;
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+        //判断用户的请求路径不应当含有参数部分。所以uri不适用。
+        String path = request.getRequestURI();
+
+        //判断是否为请求业务
+        /*
+            当我们得到本次请求路径path的值后，我们首先要查看是否为请求业务:
+            1:扫描controller包下的所有类
+            2:查看哪些被注解@Controller标注的过的类(只有被该注解标注的类才认可为业务处理类)
+            3:遍历这些类，并获取他们的所有方法，并查看哪些时业务方法
+              只有被注解@RequestMapping标注的方法才是业务方法
+            4:遍历业务方法时比对该方法上@RequestMapping中传递的参数值是否与本次请求
+              路径path值一致?如果一致则说明本次请求就应当由该方法进行处理
+              因此利用反射机制调用该方法进行处理。
+            5:如果扫描了所有的Controller中所有的业务方法，均未找到与本次请求匹配的路径
+              则说明本次请求并非处理业务，那么执行下面请求静态资源的操作
+         */
+        try {
+            File dir = new File(
+                    DispatcherServlet.class.getClassLoader()
+                            .getResource("./com/webserver/controller").toURI()
+            );
+            File[] subs = dir.listFiles(f->f.getName().endsWith(".class"));
+            for(File sub : subs){
+                String fileName = sub.getName();
+                String className = fileName.substring(0,fileName.indexOf("."));
+                Class cls = Class.forName("com.webserver.controller."+className);
+                //是否为@Controller标注的类
+                if(cls.isAnnotationPresent(Controller.class)){
+                    Method[] methods = cls.getDeclaredMethods();
+                    for(Method method : methods){
+                        //是否该方法被@RequestMapping标注
+                        if(method.isAnnotationPresent(RequestMapping.class)){
+                            RequestMapping rm = method.getAnnotation(RequestMapping.class);
+                            String value = rm.value();
+                            //本次请求路径是否与该方法@RequestMapping注解中的参数一致
+                            if(path.equals(value)){
+                                Object obj = cls.newInstance();//实例化这个Controller
+                                method.invoke(obj,request,response);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        File file = new File(staticDir, path);
+        if (file.isFile()) {//判断请求的文件真实存在且确定是一个文件(不是目录)
+            response.setContentFile(file);
+        } else {//404情况
+            response.setStatusCode(404);
+            response.setStatusReason("NotFound");
+            file = new File(staticDir, "404.html");
+            response.setContentFile(file);
+        }
+        response.addHeader("Server", "BirdServer");
+    }
+}
